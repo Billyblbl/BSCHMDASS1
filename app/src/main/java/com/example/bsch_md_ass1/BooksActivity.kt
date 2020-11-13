2dde9eaa2a47879441fe5b683a58020330ff396e
Billyblbl <billyblbl.dev@gmail.com>
2020-11-13 14:54:49 +0000

    INIT
    - Added basic android Project

---
+++ app/src/main/java/com/example/bsch_md_ass1/BooksActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bsch_md_ass1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BooksActivity : AppCompatActivity {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.books_activity)
+    }
+
+}
